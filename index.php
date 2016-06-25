ca7650d425531723c77c3846b8fb8e194890eb6a
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2016-06-25 02:06:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  <body>
  <?php
  echo '<p>Hello World; my name is php.</p>';
- echo $_SERVER['SERVER_ADDR']
+ echo '<p>My location is: ' . $_SERVER['SERVER_ADDR'] . '</p>';
   ?>
  </body>
 </html>
