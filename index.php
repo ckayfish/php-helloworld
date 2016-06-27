bc566b67a1ddcafe9d9563951d6ef6778199c7f0
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2016-06-26 20:34:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  <body>
  <?php
  echo "<p>Hello World! My name is <strong>PHP</strong> and my location is " . $_SERVER['SERVER_ADDR'] . ". Let's be friends?</p>";
- echo "<p>My php version is: ". phpversion() . "</p>"
+ echo "<p>My php version is: <strong>". phpversion() . "</strong></p>"
   ?>
  </body>
 </html>
