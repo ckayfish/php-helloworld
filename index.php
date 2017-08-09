3c066fafefb7a57a64b2ec75b26e13de7b832936
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-09 00:44:45 -0700

    Update index.php
    Add hostname to output

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  </head>
  <body>
 <?php
- echo "<p>Hello World! <strong>Address: </strong>" . $_SERVER['SERVER_ADDR'] . ". My php version is: <strong>". phpversion() . "</strong></p>";
+ echo "<p>Hello World!"."My name is: <strong>". gethostname()  ."</strong>. My address: <strong>" . $_SERVER['SERVER_ADDR'] . "</strong>. My php version is: <strong>". phpversion() . "</strong></p>";
   ?>
  </body>
 </html>
