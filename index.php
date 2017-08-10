c336e264a27eeb153d52e2339c33c3a997ae6407
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-09 23:03:13 -0700

    Update index.php
    use display:inline-block

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Says Hello</title>
  </head>
  <body>
-  <div style="background-color:powderblue;height:25px;width:640px;margin-left:5px;margin-top:5px">
+  <div style="background-color: powderblue; display: inline-block">
    <?php
  echo "<p>Hello World! I'm <strong>". gethostname()  ."</strong>, my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
   ?>
