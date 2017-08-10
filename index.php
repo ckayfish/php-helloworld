720a1308c82bed9c7bfbb1f7089b0fcb6fd8a6e4
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-09 23:52:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  </head>
  <body>
    <!-- background-color options: powderblue, palegreen - https://www.w3schools.com/cssref/css_colors.asp -->
-  <div style="display: inline-block; padding: 5px; background-color: palegreen">
+  <div style="display: inline-block; padding: 5px; background-color: powderblue">
    <?php
  echo "<p>Hello World! I'm <strong>". gethostname()  ."</strong>, my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
   ?>
