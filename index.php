7086613aabc3bb31d721a66a517ebdc7cf7ee96f
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-09 20:57:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
   <title>PHP Says Hello</title>
  </head>
  <body>
-  <div style="background-color:green">
+  <div style="background-color:powderblue;height:50px;width:81px">
+  </div>
 <?php
  echo "<p>Hello World!"."My name is: <strong>". gethostname()  ."</strong>. My address: <strong>" . $_SERVER['SERVER_ADDR'] . "</strong>. My php version is: <strong>". phpversion() . "</strong></p>";
   ?>
-  </div>
  </body>
 </html>
