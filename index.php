445f29bd8ba96078ad0d1ab7a788e4ef652d5b07
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-09 21:35:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
   <title>PHP Says Hello</title>
  </head>
  <body>
-  <div style="background-color:PaleGreen;height:50px;width:640px">
+  <div style="background-color:powderblue;height:50px;width:640px">
   </div>
 <?php
- echo "<p>Hello World! I'm <strong>". gethostname()  ."</strong> and my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
+ echo "<p>Hello World! I'm <strong>". gethostname()  ."</strong>, my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
   ?>
  </body>
 </html>
