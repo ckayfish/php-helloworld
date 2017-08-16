45c135e395d3a71412cb0f3702ac610085ad4a78
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-16 01:27:22 -0700

    Update index.php
    v13 - Hello Again

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
    <!-- background-color options: powderblue, palegreen - https://www.w3schools.com/cssref/css_colors.asp -->
   <div style="display: inline-block; padding: 5px; background-color: powderblue">
    <?php
- echo "<p>Hello World! I'm <strong>". gethostname()  ."</strong>, my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
+ echo "<p>Hello Again! I'm <strong>". gethostname()  ."</strong>, my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
   ?>
   </div>
 
