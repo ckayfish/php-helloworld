b66d3ecf63dce3913f001938974d3237e2d5b84b
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-16 01:42:45 -0700

    v14
    Color - Yellow

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <title>PHP Says Hello</title>
  </head>
  <body>
-   <!-- background-color options: powderblue, palegreen - https://www.w3schools.com/cssref/css_colors.asp -->
-  <div style="display: inline-block; padding: 5px; background-color: powderblue">
+   <!-- background-color options: powderblue, palegreen, yellow - https://www.w3schools.com/cssref/css_colors.asp -->
+  <div style="display: inline-block; padding: 5px; background-color: yellow">
    <?php
  echo "<p>Hello Again! I'm <strong>". gethostname()  ."</strong>, my address is <strong>" . $_SERVER['SERVER_ADDR'] . "</strong> and I'm served by PHP v<strong>". phpversion() . "</strong></p>";
   ?>
