780b694a1387e711aafe445ceb99fd3e77c40b2e
Curtis Kayfish <Curtis.Kayfish@gov.bc.ca>
2017-08-16 23:23:33 -0700

    Update index.php
    echo

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <div style="display: inline-block; padding: 5px; background-color: yellow">
    <?php
    $divContent= "<p>I'm <b>" . gethostname()  . "</b>, my address is <b>" . $_SERVER['SERVER_ADDR'] . "</b> and I'm served by PHP v<b>". phpversion() . "</b></p>"
-   print $divContent;
+   echo $divContent;
    ?>
    
   </div>
